0153bb5ed828dde59c84f7f216e9bf0108fd5d96
Victor Coss <gtaxl@gtaxl.net>
2024-02-25 11:05:53 -0500

    Update ZNC watcher to better version

--- services/znc/znc-certdeploy.sh
+++ services/znc/znc-certdeploy.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 # certdeploy for ZNC (IRC bouncer)
 # znc-certdeploy.sh
-# Version 2.5
-# JAN/21/2021
+# It is recommended you run this under root's crontab so it can update file permissions!
+# Version 3.0
+# FEB/24/2024
 # Victor Coss gtaxl@gtaxl.net
-# Credit: https://superuser.com/users/247052/vdr
-# This is an example for ZNC (IRC Bouncer service) before version 1.7
 # Example cronjob:
-# @reboot /home/gtaxl/certdeploy/znc-certdeploy.sh &
+# 0 * * * * /root/certdeploy/znc-certdeploy.sh
 ####################################################################################################
 ############################# CONFIGURABLE OPTIONS #################################################
 ####################################################################################################
-server=bnc.golden.gtaxl.net
-zncdir=/home/gtaxl/.znc
+server=bnc.arsenic.openbackdoor.com # The domain on the SSL cert for the server.
+cert=/var/lib/znc/znc.pem # The location of the SSL certificate. Defaults to Debian package.
+zncuser=_znc # The user ZNC runs as. Defaults to Debian package.
 ####################################################################################################
 ############################# DON'T EDIT BELOW THIS LINE ###########################################
 ####################################################################################################
-LTIME=`stat -c %Z /certdeploy/$server/fullchain.pem`
-cat /certdeploy/$server/privkey.pem /certdeploy/$server/fullchain.pem $zncdir/dhparam.pem > $zncdir/znc.pem
 
-while true    
-do
-   ATIME=`stat -c %Z /certdeploy/$server/fullchain.pem`
+scriptdir=$(realpath $(dirname $0))
 
-   if [[ "$ATIME" != "$LTIME" ]]
-   then    
-       cat /certdeploy/$server/privkey.pem /certdeploy/$server/fullchain.pem $zncdir/dhparam.pem > $zncdir/znc.pem
-       LTIME=$ATIME
-   fi
-   sleep 3600
-done
+if [ ! -f "/certdeploy/$server/fullchain.pem" ]; then
+	echo "ERROR! The certificate couldn't be found. Check the /certdeploy/ directory and what you have configured for the server variable."
+	exit 1
+fi
+
+confpath=$(dirname "$cert")
+
+if [ ! -d "$confpath" ]; then
+	echo "ERROR! Your ZNC configuration directory doesn't exist. Check the directory you set for your certificate."
+	exit 1
+fi
+
+if [ ! -f "/certdeploy/$server/dhparam.pem" ]; then
+	cd /certdeploy/$server/
+	openssl dhparam -out dhparam.pem 2048
+	chown certdeploy:$zncuser dhparam.pem
+	chmod o-r dhparam.pem
+fi
+
+update() {
+	cat /certdeploy/$server/privkey.pem /certdeploy/$server/fullchain.pem /certdeploy/$server/dhparam.pem > $cert
+}
+
+if [[ -f $scriptdir/znc-certdeploy.txt ]]
+then
+	LTIME=$(cat $scriptdir/znc-certdeploy.txt)
+	ATIME=`stat -c %Z /certdeploy/$server/fullchain.pem`
+	if [[ "$ATIME" != "$LTIME" ]]
+		then
+			echo $ATIME > $scriptdir/znc-certdeploy.txt
+			update
+		else
+			exit
+	fi
+else
+	stat -c %Z /certdeploy/$server/fullchain.pem > $scriptdir/znc-certdeploy.txt
+	update
+fi
